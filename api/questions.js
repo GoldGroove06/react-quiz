7c84b6664df084a372e1a9814c88c4363c734357
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com.>
2024-12-25 00:05:38 +0530

    changes

--- api/questions.js
+++ api/questions.js
@@ -3,7 +3,5 @@ const data = require('./data/questions.json');
 
 
 export function GET(request) {
-  return new Response(JSON.stringify([data]), {
-    headers: { "Content-Type": "application/json" },
-  });
+  return data
 }
